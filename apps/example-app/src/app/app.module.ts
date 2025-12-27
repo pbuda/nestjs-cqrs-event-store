631f2224313af63e8e4381a7433e2f0a4ec81af9
Piotr Buda <pibuda@gmail.com>
2025-12-27 22:46:26 +0100

    Updates example app to use both in memory and kurrentdb adapters

--- apps/example-app/src/app/app.module.ts
+++ apps/example-app/src/app/app.module.ts
@@ -1,14 +1,35 @@
 import { Module } from '@nestjs/common';
-import { EventStoreModule } from '@pbuda/nestjs-event-store';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import {
+  EventStoreModule,
+  IEventStoreAdapter,
+} from '@pbuda/nestjs-event-store';
 import { InMemoryEventStoreAdapter } from '@pbuda/nestjs-event-store-in-memory';
+import { KurrentDbEventStoreAdapter } from '@pbuda/nestjs-event-store-kurrentdb';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TodoModule } from './todo/todo.module';
 
 @Module({
   imports: [
-    EventStoreModule.forRoot({
-      adapter: InMemoryEventStoreAdapter,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    EventStoreModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService): IEventStoreAdapter => {
+        const adapterType = config.get<string>('EVENT_STORE_ADAPTER', 'memory');
+
+        if (adapterType === 'kurrentdb') {
+          const connectionString = config.get<string>(
+            'KURRENTDB_CONNECTION_STRING',
+            'kurrentdb://localhost:2113?tls=false',
+          );
+          return new KurrentDbEventStoreAdapter(connectionString);
+        }
+
+        return new InMemoryEventStoreAdapter();
+      },
     }),
     TodoModule,
   ],
